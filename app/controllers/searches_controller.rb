76d8e823f8d899b11e168a509bd4afbf9573b583
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-05-01 07:48:56 +0000

    [5-2]

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,7 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
   
   def search
+    @word = params[:wo]
     @range = params[:range]
     
     if @range == "User"

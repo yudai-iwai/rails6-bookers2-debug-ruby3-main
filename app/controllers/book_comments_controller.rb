f29e45033cfd75ba00e3dc6ce9162869762b6845
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-05-04 04:26:46 +0000

    [6Finish]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,17 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    # redirect_back(fallback_location: root_path)
   end
   
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
+    @book_comment = BookComment.new
+    # redirect_back(fallback_location: root_path)
   end
   
   private
